ed5bfcf18a836c7d55386bda0076446c69a88143
Sheetal Sharma <sheetalsharmaoct2003@gmail.com>
2026-06-21 07:53:32 +0530

    Time: 22 ms (74.72%), Space: 84.4 MB (19.23%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,11 +1,40 @@
 class Solution {
 public:
+
+    void countSort(auto &costs, int n){
+
+        int maxi = *max_element(costs.begin(), costs.end());
+
+        vector<int> arr(maxi+1, 0);
+
+        for(int i=0; i<n; i++){
+            arr[costs[i]]++;
+        }
+
+        costs.clear();
+
+        for(int i=0; i<=maxi; i++){
+            while(arr[i]){
+                costs.push_back(i);
+                arr[i]--;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
     int maxIceCream(vector<int>& costs, int coins) {
         int n = costs.size();
         int ans=0;
 
-        sort(costs.begin(), costs.end());
-
+        countSort(costs, n);
+       
         for(int i=0; i<costs.size(); i++){
             if(coins>=costs[i]){
                 ans++;
