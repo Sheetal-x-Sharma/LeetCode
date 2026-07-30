400e327d56610c8ded190ea898dcb245dd6c835f
Sheetal Sharma <sheetalsharmaoct2003@gmail.com>
2026-07-30 06:30:47 +0530

    Time: 0 ms (100%), Space: 8.1 MB (95.83%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        int p = n/8;
+        int r = n%8;
+
+        // 1*8 + 2*8 + 3*8 + .... + p*8 = 8*(p(1+p)/2)
+        // + (p+1)*r
+        int sum = 4*(p*(p+1)) + (p+1)*r;
+        return sum;
+    }
+};
