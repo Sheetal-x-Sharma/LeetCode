ce4b46c21cbb8e3761e970b5e3f8d74a4752c200
Sheetal Sharma <sheetalsharmaoct2003@gmail.com>
2026-07-30 06:31:43 +0530

    Time: 0 ms (100%), Space: 8.2 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -7,7 +7,7 @@ public:
 
         // 1*8 + 2*8 + 3*8 + .... + p*8 = 8*(p(1+p)/2)
         // + (p+1)*r
-        int sum = 4*(p*(p+1)) + (p+1)*r;
-        return sum;
+        //int sum = 4*p*(p+1)) + (p+1)*r; = (p+1)*(4*p+r);
+        return (p+1)*(4*p+r);;
     }
 };
