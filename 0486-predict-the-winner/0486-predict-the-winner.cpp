27c2ebf0d07c31960ab57c23fcfbc3217f0193df
Sheetal Sharma <sheetalsharmaoct2003@gmail.com>
2026-08-01 07:26:14 +0530

    Time: 0 ms (100%), Space: 10.4 MB (10.22%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -15,6 +15,27 @@ public:
         return dp[i][j] = max(take_i, take_j);
     }
 
+    int solveTab(int n, vector<int> &nums){
+
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; j<n; j++){
+                int take_i=nums[i], take_j=nums[j];
+
+                if(j-1>=0)
+                take_i = nums[i] + min(dp[i+2][j], dp[i+1][j-1]);
+                if(j-2>=0)
+                take_j = nums[j] + min(dp[i+1][j-1] , dp[i][j-2]);
+
+                dp[i][j] = max(take_i, take_j);
+            }
+        }
+
+
+        return dp[0][n-1];
+    }
+
 
 
     bool predictTheWinner(vector<int>& nums) {
@@ -23,8 +44,10 @@ public:
         for(auto i:nums)
         total += i;
 
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        int score_a = solve(0, n-1, nums, dp);
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
+        // int score_a = solve(0, n-1, nums, dp);
+
+        int score_a = solveTab(n, nums);
         int score_b = total - score_a;
         return score_a >= score_b;
     }
