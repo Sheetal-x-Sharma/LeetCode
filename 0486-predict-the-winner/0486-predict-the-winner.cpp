ace096ab73e0c61e8a2328142dc2c52e46dd5c6b
Sheetal Sharma <sheetalsharmaoct2003@gmail.com>
2026-08-01 09:12:11 +0530

    Time: 151 ms (7.81%), Space: 9.6 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,54 +1,23 @@
 class Solution {
 public:
 
-    int solve(int i, int j, vector<int> &nums, auto &dp){
+    int solve(int i, int j, auto &nums){
 
         if(i>j)
         return 0;
-        if(dp[i][j] != -1)
-        return dp[i][j];
 
-        int take_i = nums[i] + min(solve(i+2, j, nums, dp), solve(i+1, j-1, nums, dp));
-
-        int take_j = nums[j] + min(solve(i+1, j-1, nums, dp), solve(i, j-2, nums, dp));
-
-        return dp[i][j] = max(take_i, take_j);
-    }
-
-    int solveTab(int n, vector<int> &nums){
-
-        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
-
-        for(int i=n-1; i>=0; i--){
-            for(int j=i; j<n; j++){
-                int take_i=nums[i], take_j=nums[j];
-
-                if(j-1>=0)
-                take_i = nums[i] + min(dp[i+2][j], dp[i+1][j-1]);
-                if(j-2>=0)
-                take_j = nums[j] + min(dp[i+1][j-1] , dp[i][j-2]);
-
-                dp[i][j] = max(take_i, take_j);
-            }
-        }
+        int op1 = nums[i] - solve(i+1, j, nums);
+        int op2 = nums[j] - solve(i, j-1, nums);
 
 
-        return dp[0][n-1];
+        return max(op1, op2);
     }
 
 
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        int total = 0;
-        for(auto i:nums)
-        total += i;
-
-        // vector<vector<int>> dp(n, vector<int>(n, -1));
-        // int score_a = solve(0, n-1, nums, dp);
-
-        int score_a = solveTab(n, nums);
-        int score_b = total - score_a;
-        return score_a >= score_b;
+        int diff = solve(0, n-1, nums);
+        return diff>=0?true:false;
     }
 };
