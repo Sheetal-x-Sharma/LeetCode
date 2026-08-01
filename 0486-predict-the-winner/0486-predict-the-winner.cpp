181642eb53a3c063b8d33a935a095c27589b95ba
Sheetal Sharma <sheetalsharmaoct2003@gmail.com>
2026-08-01 07:16:45 +0530

    Time: 0 ms (100%), Space: 10.2 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
 
-    int solve(int i, int j, vector<int> &nums){
+    int solve(int i, int j, vector<int> &nums, auto &dp){
 
         if(i>j)
         return 0;
+        if(dp[i][j] != -1)
+        return dp[i][j];
 
-        int take_i = nums[i] + min(solve(i+2, j, nums), solve(i+1, j-1, nums));
+        int take_i = nums[i] + min(solve(i+2, j, nums, dp), solve(i+1, j-1, nums, dp));
 
-        int take_j = nums[j] + min(solve(i+1, j-1, nums), solve(i, j-2, nums));
+        int take_j = nums[j] + min(solve(i+1, j-1, nums, dp), solve(i, j-2, nums, dp));
 
-        return max(take_i, take_j);
+        return dp[i][j] = max(take_i, take_j);
     }
 
 
@@ -20,8 +22,9 @@ public:
         int total = 0;
         for(auto i:nums)
         total += i;
-        
-        int score_a = solve(0, n-1, nums);
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        int score_a = solve(0, n-1, nums, dp);
         int score_b = total - score_a;
         return score_a >= score_b;
     }
